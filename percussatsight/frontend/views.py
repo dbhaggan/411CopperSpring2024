4ee767f778ff58813ba8148c889ada4a2f500d94
AsianWarrior <95550980+AsianWarrior@users.noreply.github.com>
2024-01-28 19:06:55 -0500

    Update views.py
    Including homepage and sheetspages

--- percussatsight/frontend/views.py
+++ percussatsight/frontend/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.render import loader
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello world")
+
+def homepage(request):
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
+
+def sheetspage(request):
+    template = loader.get_template('sheetspage.html')
+    return HttpResponse(template.render())
