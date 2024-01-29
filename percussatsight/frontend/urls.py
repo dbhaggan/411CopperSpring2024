6d2769bbb4a2070917cff1c33ea46c071bdacaf3
dbhaggan <dbhag001@odu.edu>
2024-01-29 13:13:35 -0500

    added templates and url paths

--- percussatsight/frontend/urls.py
+++ percussatsight/frontend/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("home", view.homepage, name="home"),
-    path("sheets", view.sheetspage, name="sheets")
+    path("home", views.homepage, name="home"),
+    path("sheets", views.sheetspage, name="sheets"),
+    path("login", views.login, name="login"),
+    path("signup", views.signup, name="signup"),
 ]
