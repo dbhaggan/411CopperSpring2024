10e0f04ba7166432c9aaba77a8263652513c25ef
AsianWarrior <95550980+AsianWarrior@users.noreply.github.com>
2024-02-05 22:41:22 -0500

    Update urls.py

--- percussatsight/frontend/urls.py
+++ percussatsight/frontend/urls.py
@@ -4,12 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("home", views.homepage, name="home"),
-<<<<<<< HEAD
-    path("sheets", views.sheetspage, name="sheets")
-]
-=======
+    path("sheets", views.sheetspage, name="sheets"),
     path("sheets", views.sheetspage, name="sheets"),
     path("login", views.login, name="login"),
     path("signup", views.signup, name="signup"),
 ]
->>>>>>> f4d0010e3d15b7681c82a7b4e5fa078e66766571
+
