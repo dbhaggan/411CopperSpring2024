950e3fba8cd9ca995a12420b0584269efb8483e6
dbhaggan <dbhag001@odu.edu>
2024-02-28 11:32:16 -0500

    start models skeleton

--- percussatsight/frontend/models.py
+++ percussatsight/frontend/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class User(models.Model):
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
+    userName = models.CharField(max_length=255)
+    passWord = models.CharField(max_length=255)
+    emailAddress = models.CharField(max_length=255)
+
+class User_Progress(models.Model):
+
+class Sheet_Music(models.Model):
+
+class Feedback (models.Model):
+
+class Rhythm (models.Model):
+
+class Practice_SEssion (models.Model):
