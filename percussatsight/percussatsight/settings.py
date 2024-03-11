93fe72f0964ef403fe6bf543af2a494e3392e505
AsianWarrior <thephat0209@gmail.com>
2024-03-10 21:29:36 -0400

    updating docker components for django

--- percussatsight/percussatsight/settings.py
+++ percussatsight/percussatsight/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'frontend',
     'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- percussatsight/startup.sh
+++ percussatsight/startup.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-python3 manage.py runserver 0.0.0.0:80
+python3 manage.py runserver 0.0.0.0:8080
