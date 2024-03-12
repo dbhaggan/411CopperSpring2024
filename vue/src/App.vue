e4b05b51dfb81a1a51177cf611445607c9da609a
dbhaggan <dbhag001@odu.edu>
2024-03-12 11:22:13 -0400

    update App.vue to call vue components

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,11 +5,27 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import app-settings from './components/app-settings.vue'
+import Feedback from './components/Feedback.vue'
+import generator-settings from './components/generator-settings.vue'
+import homepage from './components/homepage.vue'
+import instrument-selection from './components/instrument-selection.vue'
+import login from './components/login.vue'
+import practice from './components/practice.vue'
+import signup from './components/signup.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    app-settings,
+    Feedback,
+    generator-settings,
+    homepage,
+    instrument-selection,
+    login,
+    practice,
+    signup
   }
 }
 </script>
