cc53ffba4149629b6979d2dfe1d2f929c329cad4
JeremyS222 <115397449+JeremyS222@users.noreply.github.com>
2024-03-18 16:45:43 -0400

    Merging fix vuejs branch to main (#4)
    * Finish integrating Vue and Django
    
    * Testing pushing to new branch (that's not main)
    
    ---------
    
    Co-authored-by: jacobcook135 <jacobcook135@gmail.com>

--- vue/src/components/PracticePage.vue
+++ vue/src/components/PracticePage.vue
@@ -1,48 +1,46 @@
 <template>
-<!DOCTYPE html>
-<html>
-<body style="background-color:#003e7d">
-
-<div>
-  <link href="./practice.css" rel="stylesheet" />
-  <div class="practice-container">
-    <div class="practice-container1">
-      <button type="button" class="practice-button button">
-        <span class="practice-text">
-          <span>Practice</span>
-          <br />
-        </span>
-      </button>
-      <button type="button" class="practice-button1 button">
-        <span class="practice-text03">
-          <span>Collaborative Learning</span>
-          <br />
-        </span>
-      </button>
-      <button type="button" class="practice-button2 button">
-        <span>
-          <span>Settings</span>
-          <br />
-        </span>
-      </button>
-      <button type="button" class="practice-button3 button">
-        <span class="practice-text09">
-          <span>Home</span>
-          <br />
-        </span>
-      </button>
-    </div>
-    <button type="button" class="practice-button4 button">
-      <span>
-        <br />
-        <br />
-      </span>
-    </button>
-  </div>
-</div>
-
-</body>
-</html>
+  <!DOCTYPE html>
+  <html>
+    <body style="background-color: #003e7d">
+      <div>
+        <link href="./practice.css" rel="stylesheet" />
+        <div class="practice-container">
+          <div class="practice-container1">
+            <button type="button" class="practice-button button">
+              <span class="practice-text">
+                <span>Practice</span>
+                <br />
+              </span>
+            </button>
+            <button type="button" class="practice-button1 button">
+              <span class="practice-text03">
+                <span>Collaborative Learning</span>
+                <br />
+              </span>
+            </button>
+            <button type="button" class="practice-button2 button">
+              <span>
+                <span>Settings</span>
+                <br />
+              </span>
+            </button>
+            <button type="button" class="practice-button3 button">
+              <span class="practice-text09">
+                <span>Home</span>
+                <br />
+              </span>
+            </button>
+          </div>
+          <button type="button" class="practice-button4 button">
+            <span>
+              <br />
+              <br />
+            </span>
+          </button>
+        </div>
+      </div>
+    </body>
+  </html>
 </template>
 
 <style>
@@ -64,7 +62,7 @@
   align-items: center;
   flex-direction: column;
   justify-content: flex-start;
-  background-color: #0055FF;
+  background-color: #0055ff;
 }
 .practice-button {
   color: rgb(128, 170, 255);
@@ -72,7 +70,7 @@
   height: 90px;
   opacity: 1;
   font-size: 24px;
-  border-color: #80AAFF;
+  border-color: #80aaff;
   border-width: 3px;
   text-transform: capitalize;
   background-color: rgb(0, 85, 255);
@@ -85,7 +83,7 @@
   width: 181px;
   height: 90px;
   opacity: 1;
-  border-color: #80AAFF;
+  border-color: #80aaff;
   border-width: 3px;
   background-color: rgb(0, 85, 255);
 }
@@ -99,7 +97,7 @@
   height: 90px;
   opacity: 1;
   font-size: 24px;
-  border-color: #80AAFF;
+  border-color: #80aaff;
   border-width: 3px;
   background-color: rgb(0, 85, 255);
 }
@@ -111,7 +109,7 @@
   height: 90px;
   opacity: 1;
   position: absolute;
-  border-color: #80AAFF;
+  border-color: #80aaff;
   border-width: 3px;
   background-color: rgb(0, 85, 255);
 }
