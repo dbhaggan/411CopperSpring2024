d1a74e347ba024a642f2bd6c449db20b5c666a7e
Swafu1 <swafu001@odu.edu>
2024-03-24 15:46:18 -0400

    updating registered models

--- percussatsight/frontend/admin.py
+++ percussatsight/frontend/admin.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
-#from .newmodels import UserProfile, UserType, SchoolProfile, SchoolUser, UserInformation, Subscription, SchoolInformation, Payment, AppSettings, Sheets, Performances, Instruments, Engagements, Feedbacks
-# Register your models here.
+# from .models import User, SheetMusic, Note, SheetMusicNote, UserProgress, Feedback, Instrument, InstrumentNote, PracticeSession
 
-#admin.site.register(UserProfile)
-#admin.site.register(UserType)
-#admin.site.register(SchoolProfile)
-#admin.site.register(SchoolUser)
-#admin.site.register(UserInformation)
-#admin.site.register(Subscription)
-#admin.site.register(SchoolInformation)
-#admin.site.register(Payment)
-#admin.site.register(AppSettings)
-#admin.site.register(Sheets)
-#admin.site.register(Performances)
-#admin.site.register(Instruments)
-#admin.site.register(Engagements)
-#admin.site.register(Feedbacks)
+# admin.site.register(User)
+# admin.site.register(SheetMusic)
+# admin.site.register(Note)
+# admin.site.register(SheetMusicNote)
+# admin.site.register(UserProgress)
+# admin.site.register(Feedback)
+# admin.site.register(Instrument)
+# admin.site.register(InstrumentNote)
+# admin.site.register(PracticeSession)
