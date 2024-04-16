de7e8f6a617eb7739ed758ca523bbbea591e64dd
phnguyen <pnguy004@odu.edu>
2024-04-16 13:23:12 -0400

    removing templates from django views and urls

--- percussatsight/frontend/urls.py
+++ percussatsight/frontend/urls.py
@@ -8,11 +8,6 @@ router.register(r'user', userViewSet, 'user')
 router.register(r'instrument', instrumentViewSet, 'instrument')
 
 urlpatterns = [
-    path("home", homepage, name="home"),
-    path("sheets", sheetspage, name="sheets"),
-    path("", loginpage, name="login"),
-    path("login", loginpage, name="login"),
-    path("signup", signuppage, name="signup"),
     path("api/", include(router.urls)),
     #path("api", include('rest_framework.urls')),
 ]
