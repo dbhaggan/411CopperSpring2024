eadec61318eacb144d9c43a2c3b35891822f9b27
dbhaggan <dbhag001@odu.edu>
2024-04-20 15:27:01 -0400

    changes from mentor feedback

--- vue/src/components/Login.vue
+++ vue/src/components/Login.vue
@@ -1,77 +1,46 @@
 <template>
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <title>login - PercussAtSight</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="../assets/style.css" />
-      <link rel="stylesheet" href="../assets/login.css" />
-    </head>
-    <body>
-      <div class="login-container">
-        <div class="login-container1">
-          <div class="login-container2">
-            <a href="homepage.html" class="login-navlink">
-              <img
-                src="../assets/PercussAtSight_Logo.png"
-                alt="logo"
-                class="login-image"
-              />
-            </a>
-          </div>
-          <header class="login-navbar-interactive">
-            <div class="login-desktop-menu">
-              <div class="login-buttons">
-                <!-- <a href="login.html" class="login-login button">Login</a>-->
-                <RouterLink to="/Login" class="login-login button">Login</RouterLink>
-              </div>
-              <!--<a href="signup.html" name="sign-up" class="login-sign-up button">Sign-up</a>-->
-              <RouterLink to="/SignUp" name="sign-up" class="login-sign-up button">Sign Up</RouterLink>
-              <button name="practice" class="login-practice button">
-                Practice
-              </button>
-            </div>
-          </header>
-          <form class="login-form">
-            <label class="login-text5">Username:</label>
-            <input
-              type="text"
-              placeholder="Username"
-              class="login-textinput input"
-            />
-
-            <label class="login-text6">Password:</label>
-            <input
-              type="password"
-              placeholder="Password"
-              class="login-textinput1 input"
-            />
-
-            <img
-              alt="logo"
-              src="../assets/PercussAtSight_Logo.png"
-              class="login-image1"
-            />
-
-            <button type="submit" name="log-in" class="login-button button">
-              Log-In
-            </button>
-          </form>
-        </div>
-      </div>
-    </body>
-  <RouterView />
-  </html>
+  <div class="login-container"
+    ><div class="login-container1"
+      ><div class="login-container2"
+        ><router-link to="/" class="login-navlink"
+          ><img
+            alt="image"
+            src="../assets/PercussAtSight_Logo.png"
+            class="login-image" /></router-link></div
+      ><div class="login-container3"
+        ><input
+          type="text"
+          placeholder="Password"
+          class="login-textinput input"
+        /><input
+          type="text"
+          placeholder="Username "
+          class="login-textinput1 input"
+        /><label class="login-text">Username:</label
+        ><label class="login-text1">Password:</label
+        ><button type="submit" class="login-button button">Log-In</button></div
+      ></div
+    ></div
+  >
 </template>
 
 <script>
-  export default {
-    name: 'LoginPage',
-  }
+export default {
+  name: 'Login',
+  props: {},
+  metaInfo: {
+    title: 'login - PercussAtSight',
+    meta: [
+      {
+        property: 'og:title',
+        content: 'login - PercussAtSight',
+      },
+    ],
+  },
+}
 </script>
 
-<style>
+<style scoped>
 .login-container {
   width: 100%;
   display: flex;
@@ -88,7 +57,6 @@
   min-height: 100vh;
   align-items: center;
   flex-direction: column;
-  background-image: white;
 }
 .login-container2 {
   flex: 0 0 auto;
@@ -97,256 +65,65 @@
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: white;
+  background-color: var(--dl-color-gray-white);
 }
 .login-navlink {
   display: contents;
 }
 .login-image {
-  width: 200px;
+  width: 400px;
   object-fit: cover;
   text-decoration: none;
 }
-.login-navbar-interactive {
-  width: 100%;
-  display: flex;
-  align-items: center;
-  padding-top: var(--dl-space-space-twounits);
-  padding-left: var(--dl-space-space-threeunits);
-  padding-right: var(--dl-space-space-threeunits);
-  padding-bottom: var(--dl-space-space-twounits);
-  justify-content: space-between;
-  background-color: white;
-}
-.login-desktop-menu {
-  flex: 1;
-  display: flex;
-  justify-content: space-between;
-  background-color: white;
-}
-.login-buttons {
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-between;
-}
-.login-login {
-  color: white;
-  width: var(--dl-size-size-large);
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
-  text-decoration: none;
-  background-color: #1e2f86;
-}
-.login-sign-up {
-  color: white;
-  width: var(--dl-size-size-large);
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
-  text-decoration: none;
-  background-color: #1e2f86;
-}
-.login-practice {
-  color: white;
-  width: var(--dl-size-size-large);
-  height: 100%;
-  align-self: stretch;
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
-  background-color: #1e2f86;
-}
-.login-burger-menu {
-  display: none;
-}
-.login-icon {
-  width: var(--dl-size-size-medium);
-  height: var(--dl-size-size-xsmall);
-}
-.login-mobile-menu {
-  top: 0px;
-  left: 0px;
-  width: 100%;
-  height: 100vh;
-  display: none;
-  padding: 32px;
-  z-index: 100;
-  position: absolute;
-  flex-direction: column;
-  justify-content: space-between;
-  background-color: #fff;
-}
-.login-nav {
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-}
-.login-top {
-  width: 100%;
-  display: flex;
-  align-items: center;
-  margin-bottom: var(--dl-space-space-threeunits);
-  justify-content: space-between;
-}
-.login-logo {
-  height: 2rem;
-}
-.login-close-menu {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.login-icon02 {
-  width: var(--dl-size-size-xsmall);
-  height: var(--dl-size-size-xsmall);
-}
-.login-links {
-  flex: 0 0 auto;
-  display: flex;
-  align-self: flex-start;
-  align-items: flex-start;
-  flex-direction: column;
-}
-.login-text {
-  margin-bottom: var(--dl-space-space-unit);
-}
-.login-text1 {
-  margin-bottom: var(--dl-space-space-unit);
-}
-.login-text2 {
-  margin-bottom: var(--dl-space-space-unit);
-}
-.login-text3 {
-  margin-bottom: var(--dl-space-space-unit);
-}
-.login-text4 {
-  margin-bottom: var(--dl-space-space-unit);
-}
-.login-buttons1 {
-  display: flex;
-  margin-top: var(--dl-space-space-unit);
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-between;
-}
-.login-login1 {
-  margin-right: var(--dl-space-space-twounits);
-}
-.login-icon04 {
-  width: var(--dl-size-size-xsmall);
-  height: var(--dl-size-size-xsmall);
-  margin-right: var(--dl-space-space-twounits);
-}
-.login-icon06 {
-  width: var(--dl-size-size-xsmall);
-  height: var(--dl-size-size-xsmall);
-  margin-right: var(--dl-space-space-twounits);
-}
-.login-icon08 {
-  width: var(--dl-size-size-xsmall);
-  height: var(--dl-size-size-xsmall);
-}
-.login-form {
-  width: 100%;
-  height: 769px;
+.login-container3 {
+  width: 760px;
+  height: 727px;
   display: flex;
   position: relative;
+  align-self: center;
   align-items: center;
-  justify-content: flex-end;
-  background-color: var(--dl-color-gray-white);
+  flex-direction: column;
+  justify-content: center;
 }
 .login-textinput {
-  top: 204px;
-  left: 473px;
+  top: 395px;
+  left: 0px;
+  right: 0px;
+  width: 252px;
+  height: 72px;
+  margin: auto;
   position: absolute;
-  align-self: center;
 }
 .login-textinput1 {
-  top: 366px;
-  left: 473px;
+  top: 195px;
+  left: 0px;
+  right: 0px;
+  width: 252px;
+  height: 72px;
+  margin: auto;
   position: absolute;
-  align-self: center;
 }
-.login-text5 {
-  top: 182px;
-  left: 475px;
-  color: #000000;
+.login-text {
+  top: 151px;
+  left: 298px;
+  margin: auto;
   position: absolute;
-  align-self: center;
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
+  font-size: 32px;
 }
-.login-text6 {
-  top: 349px;
-  left: 476px;
+.login-text1 {
+  top: 358px;
+  left: 298px;
+  margin: auto;
   position: absolute;
-  align-self: center;
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
+  font-size: 32px;
 }
-.login-image1 {
-  top: 111px;
-  right: 2px;
-  width: 658px;
-  height: 396px;
-  position: absolute;
-  align-self: center;
-  object-fit: cover;
-}
-
 .login-button {
-  top: 430px;
-  left: 475px;
+  top: 503px;
+  left: 0px;
+  right: 0px;
+  width: 164px;
+  height: 48px;
+  margin: auto;
   position: absolute;
-}
-
-@media (max-width: 991px) {
-  .login-desktop-menu {
-    display: none;
-  }
-  .login-burger-menu {
-    display: flex;
-  }
-}
-@media (max-width: 767px) {
-  .login-navbar-interactive {
-    padding-left: var(--dl-space-space-twounits);
-    padding-right: var(--dl-space-space-twounits);
-  }
-  .login-burger-menu {
-    align-items: center;
-    justify-content: center;
-  }
-  .login-text {
-    margin-bottom: var(--dl-space-space-unit);
-  }
-  .login-text1 {
-    margin-left: 0;
-    margin-bottom: var(--dl-space-space-unit);
-  }
-  .login-text2 {
-    margin-left: 0;
-    margin-bottom: var(--dl-space-space-unit);
-  }
-  .login-text3 {
-    margin-left: 0;
-    margin-bottom: var(--dl-space-space-unit);
-  }
-  .login-text4 {
-    margin-left: 0;
-    margin-bottom: var(--dl-space-space-unit);
-  }
-}
-@media (max-width: 479px) {
-  .login-navbar-interactive {
-    padding: var(--dl-space-space-unit);
-  }
-  .login-mobile-menu {
-    padding: 16px;
-  }
 }
 </style>
--- vue/src/components/SignUp.vue
+++ vue/src/components/SignUp.vue
@@ -1,68 +1,51 @@
 <template>
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="utf-8">
-      <title>signup - PercussAtSight</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="../assets/style.css">
-      <link rel="stylesheet" href="../assets/signup.css">
-  </head>
-  <body>
-    <div class="signup-container">
-      <div class="signup-container1">
-          <div class="signup-container2">
-              <!--<a href="homepage.html" class="login-navlink">-->
-                <RouterLink to="/home" class="login-navlink">
-                  <img src="../assets/PercussAtSight_Logo.png" alt="logo" class="login-image">
-                </RouterLink>  
-              <!--</a>-->
-          </div>
-          <header class="signup-navbar-interactive">
-              <div class="signup-desktop-menu">
-                  <div class="signup-buttons">
-                      <!--<a href="login.html" class="signup-login button">Login</a>-->
-                      <RouterLink to="/login" name="login" class="signup-login button">Login</RouterLink>
-                  </div>
-                  <!--<a href="signup.html" name="sign-up" class="signup-sign-up button">Sign-up</a>-->\
-                  <RouterLink to="/SignUp" name="sign-up" class="signup-sign-up button">Sign Up</RouterLink>
-                  <button name="practice" class="login-practice button">
-                    <RouterLink to="/practice" name="practice">Practice</RouterLink>
-                  </button>
-              </div>
-          </header>
-          <form class="signup-form">
-              <label class="signup-text5">First Name:</label>
-              <input type="text" placeholder="First Name" class="signup-textinput input" />
-  
-              <label class="signup-text6">Last Name:</label>
-              <input type="text" placeholder="Last Name" class="signup-textinput1 input" />
-  
-              <label class="signup-text7">Email:</label>
-              <input type="email" placeholder="Email" class="signup-textinput2 input" />
-  
-              <img
-              alt="logo"
-              src="../assets/PercussAtSight_Logo.png"
-              class="login-image1"
-              />
-  
-              <button name="signup" type="submit" class="signup-signup button">Sign Up</button>
-          </form>
-      </div>
-    </div>
-  </body>
-  <RouterView />
-  </html>
+  <div class="signup-container"
+    ><div class="signup-container1"
+      ><div class="signup-container2"
+        ><router-link to="/" class="signup-navlink"
+          ><img
+            alt="image"
+            src="../assets/PercussAtSight_Logo.png"
+            class="signup-image" /></router-link></div
+      ><div class="signup-container3"
+        ><input
+          type="text"
+          placeholder="Last Name"
+          class="signup-textinput input"
+        /><input
+          type="text"
+          placeholder="First Name"
+          class="signup-textinput1 input"
+        /><input
+          type="text"
+          placeholder="Email "
+          class="signup-textinput2 input"
+        /><label class="signup-text">First Name:</label
+        ><label class="signup-text1">Email:</label
+        ><label class="signup-text2">Password:</label
+        ><button type="submit" class="signup-button button">Sign-Up</button></div
+      ></div
+    ></div
+  >
 </template>
 
 <script>
-  export default {
-    name: "SignupPage"
-  }
+export default {
+  name: 'Signup',
+  props: {},
+  metaInfo: {
+    title: 'signup - PercussAtSight',
+    meta: [
+      {
+        property: 'og:title',
+        content: 'signup - PercussAtSight',
+      },
+    ],
+  },
+}
 </script>
 
-<style>
+<style scoped>
 .signup-container {
   width: 100%;
   display: flex;
@@ -79,7 +62,6 @@
   min-height: 100vh;
   align-items: center;
   flex-direction: column;
-  background-image: white;
 }
 .signup-container2 {
   flex: 0 0 auto;
@@ -88,270 +70,77 @@
   display: flex;
   align-items: center;
   justify-content: center;
-  background-image: white;
+  background-color: var(--dl-color-gray-white);
 }
 .signup-navlink {
   display: contents;
 }
 .signup-image {
-  width: 200px;
+  width: 400px;
   object-fit: cover;
   text-decoration: none;
 }
-.signup-navbar-interactive {
-  width: 100%;
-  display: flex;
-  align-items: center;
-  padding-top: var(--dl-space-space-twounits);
-  padding-left: var(--dl-space-space-threeunits);
-  padding-right: var(--dl-space-space-threeunits);
-  padding-bottom: var(--dl-space-space-twounits);
-  justify-content: space-between;
-  background-image: white;
-}
-.signup-desktop-menu {
-  flex: 1;
-  display: flex;
-  justify-content: space-between;
-  background-image: white;
-}
-.signup-buttons {
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-between;
-}
-.signup-login {
-  color: white;
-  width: var(--dl-size-size-large);
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
-  text-decoration: none;
-  background-color: #1e2f86;
-}
-.signup-sign-up {
-  color: white;
-  width: var(--dl-size-size-large);
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
-  text-decoration: none;
-  background-color: #1e2f86;
-}
-.signup-practice {
-  color: white;
-  width: var(--dl-size-size-large);
-  height: 100%;
-  align-self: stretch;
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
-  background-color: #1e2f86;
-}
-.signup-burger-menu {
-  display: none;
-}
-.signup-icon {
-  width: var(--dl-size-size-medium);
-  height: var(--dl-size-size-xsmall);
-}
-.signup-mobile-menu {
-  top: 0px;
-  left: 0px;
-  width: 100%;
-  height: 100vh;
-  display: none;
-  padding: 32px;
-  z-index: 100;
-  position: absolute;
-  flex-direction: column;
-  justify-content: space-between;
-  background-color: #fff;
-}
-.signup-nav {
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-}
-.signup-top {
-  width: 100%;
-  display: flex;
-  align-items: center;
-  margin-bottom: var(--dl-space-space-threeunits);
-  justify-content: space-between;
-}
-.signup-logo {
-  height: 2rem;
-}
-.signup-close-menu {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.signup-icon02 {
-  width: var(--dl-size-size-xsmall);
-  height: var(--dl-size-size-xsmall);
-}
-.signup-links {
-  flex: 0 0 auto;
-  display: flex;
-  align-self: flex-start;
-  align-items: flex-start;
-  flex-direction: column;
-}
-.signup-text {
-  margin-bottom: var(--dl-space-space-unit);
-}
-.signup-text1 {
-  margin-bottom: var(--dl-space-space-unit);
-}
-.signup-text2 {
-  margin-bottom: var(--dl-space-space-unit);
-}
-.signup-text3 {
-  margin-bottom: var(--dl-space-space-unit);
-}
-.signup-text4 {
-  margin-bottom: var(--dl-space-space-unit);
-}
-.signup-buttons1 {
-  display: flex;
-  margin-top: var(--dl-space-space-unit);
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-between;
-}
-.signup-login1 {
-  margin-right: var(--dl-space-space-twounits);
-}
-.signup-icon04 {
-  width: var(--dl-size-size-xsmall);
-  height: var(--dl-size-size-xsmall);
-  margin-right: var(--dl-space-space-twounits);
-}
-.signup-icon06 {
-  width: var(--dl-size-size-xsmall);
-  height: var(--dl-size-size-xsmall);
-  margin-right: var(--dl-space-space-twounits);
-}
-.signup-icon08 {
-  width: var(--dl-size-size-xsmall);
-  height: var(--dl-size-size-xsmall);
-}
-.signup-form {
-  width: 100%;
-  height: 769px;
+.signup-container3 {
+  width: 760px;
+  height: 727px;
   display: flex;
   position: relative;
+  align-self: center;
   align-items: center;
+  flex-direction: column;
   justify-content: center;
 }
 .signup-textinput {
-  top: 204px;
-  left: 473px;
+  top: 299px;
+  left: 256px;
+  width: 252px;
+  height: 72px;
+  margin: auto;
   position: absolute;
-  align-self: center;
 }
 .signup-textinput1 {
-  top: 299px;
-  left: 473px;
+  top: 137px;
+  left: 262px;
+  width: 252px;
+  height: 72px;
+  margin: auto;
   position: absolute;
-  align-self: center;
 }
 .signup-textinput2 {
-  top: 393px;
-  left: 473px;
-  position: absolute;
-  align-self: center;
-}
-.signup-text5 {
-  top: 182px;
-  left: 475px;
-  color: rgb(0, 0, 0);
-  position: absolute;
-  align-self: center;
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
-}
-.signup-text6 {
-  top: 274px;
-  left: 475px;
-  position: absolute;
-  align-self: center;
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
-}
-.signup-text7 {
-  top: 370px;
-  left: 476px;
-  color: rgb(0, 0, 0);
-  position: absolute;
-  align-self: center;
-  font-style: normal;
-  font-family: "Arial";
-  font-weight: 700;
-}
-.signup-image1 {
-  top: 111px;
-  right: 2px;
-  width: 658px;
-  height: 396px;
-  position: absolute;
-  align-self: center;
-  object-fit: cover;
-}
-.signup-signup {
-  left: 472px;
-  bottom: 282px;
+  top: 457px;
+  left: 256px;
+  width: 252px;
+  height: 72px;
+  margin: auto;
   position: absolute;
 }
-@media(max-width: 991px) {
-  .signup-desktop-menu {
-    display: none;
-  }
-  .signup-burger-menu {
-    display: flex;
-  }
+.signup-text {
+  top: 91px;
+  left: 298px;
+  margin: auto;
+  position: absolute;
+  font-size: 32px;
 }
-@media(max-width: 767px) {
-  .signup-navbar-interactive {
-    padding-left: var(--dl-space-space-twounits);
-    padding-right: var(--dl-space-space-twounits);
-  }
-  .signup-burger-menu {
-    align-items: center;
-    justify-content: center;
-  }
-  .signup-text {
-    margin-bottom: var(--dl-space-space-unit);
-  }
-  .signup-text1 {
-    margin-left: 0;
-    margin-bottom: var(--dl-space-space-unit);
-  }
-  .signup-text2 {
-    margin-left: 0;
-    margin-bottom: var(--dl-space-space-unit);
-  }
-  .signup-text3 {
-    margin-left: 0;
-    margin-bottom: var(--dl-space-space-unit);
-  }
-  .signup-text4 {
-    margin-left: 0;
-    margin-bottom: var(--dl-space-space-unit);
-  }
+.signup-text1 {
+  top: 401px;
+  left: 296px;
+  margin: auto;
+  position: absolute;
+  font-size: 32px;
 }
-@media(max-width: 479px) {
-  .signup-navbar-interactive {
-    padding: var(--dl-space-space-unit);
-  }
-  .signup-mobile-menu {
-    padding: 16px;
-  }
+.signup-text2 {
+  top: 248px;
+  left: 302px;
+  margin: auto;
+  position: absolute;
+  font-size: 32px;
 }
-
+.signup-button {
+  top: 559px;
+  left: 300px;
+  width: 164px;
+  height: 48px;
+  margin: auto;
+  position: absolute;
+}
 </style>
