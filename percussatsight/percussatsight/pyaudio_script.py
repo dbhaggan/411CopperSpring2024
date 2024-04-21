56e657825bbb1a391f1eae3c1a733a578d43b476
Ismail <imismail2k@gmail.com>
2024-04-20 20:23:03 -0400

    final pyaudio script for demo

---
+++ percussatsight/percussatsight/pyaudio_script.py
@@ -0,0 +1,36 @@
+import pyaudio
+import wave
+
+# defining th audio parameters
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+FILE_PATH = "music_sample.wav"
+
+# initializing pyaudio
+p = pyaudio.PyAudio()
+
+# opening the audio file
+wf = wave.open(FILE_PATH, 'rb')
+
+# opening stream
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+
+print("* Playing audio...")
+
+# reading data from file and playing
+data = wf.readframes(CHUNK)
+while data:
+    stream.write(data)
+    data = wf.readframes(CHUNK)
+
+print("* Finished playing")
+
+# stopping stream
+stream.stop_stream()
+stream.close()
+p.terminate()
